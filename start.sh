b8c1a69b20e4e6d310cd6f344ffcee79815e48f7
skcreator7 <118694333+skcreator7@users.noreply.github.com>
2022-12-26 16:24:00 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 echo "Cloning Repo...."
-git clone https://github.com/Cyniteofficial/MdiskSearchBotV2.git /MdiskSearchBotV2
-cd /MdiskSearchBotV2
+git clone https://github.com/skcreator7
+/sk_mdisk.git /sk_mdisk
+cd /sk_mdisk
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
