df051851c3babfa05e4610650df815b05e384a3a
Prasgfqyqhn <110588740+Prasgfqyqhn@users.noreply.github.com>
2023-01-20 12:25:00 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/Cyniteofficial/MdiskSearchBotV2.git /MdiskSearchBotV2
-cd /MdiskSearchBotV2
+git clone https://github.com/pnxfficial/pnxSearchBotV2.git /pnxSearchBotV2
+cd /pnxSearchBotV2
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
