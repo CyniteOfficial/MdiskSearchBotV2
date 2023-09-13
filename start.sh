c3b18ce7380a6e627b3ba7ee12655f3d47abf5ee
thebeatsofficial <144920361+thebeatsofficial@users.noreply.github.com>
2023-09-13 19:10:30 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/Cyniteofficial/MdiskSearchBotV2.git /MdiskSearchBotV2
+git clone https://github.com/thebeatsofficial/MdiskSearchBotV2.git /MdiskSearchBotV2
 cd /MdiskSearchBotV2
 pip3 install -r requirements.txt
 echo "Starting Bot...."
